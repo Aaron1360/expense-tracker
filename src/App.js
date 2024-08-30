c242e584b84a9ba5e9b2c2e5d7e3b1ab2879dc66
Aaron Moreno Villeda <aaronmvilleda@gmail.com>
2024-08-30 17:39:49 -0600

    Fixing resposinve design

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import LoginPage from './LoginPage';
 
 function App() {
-  return (
-    <div className="App bg-dark text-white min-vh-100 d-flex flex-column justify-content-center align-items-center">
+  return (//bg-dark text-white min-vh-100 d-flex flex-column justify-content-center align-items-center
+    <div className="App">
       <LoginPage />
     </div>
   );
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,65 +1,136 @@
-import React from 'react';
+import { useState } from 'react';
 import { Container, Row, Col, Form, Button, InputGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 const LoginPage = () => {
-    return (
-        <Container className="mt-5 min-vh-100 d-flex flex-column" >
-            <Row className="justify-content-md-center">
-                <Col md="4" className="text-center justify-content-evenly">
-                    {/* Logo */}
-                    <img 
-                        src="devmtn.png"
-                        alt="Logo" 
-                        className="mb-4" 
-                        style={{ width: '100px' }}
-                    />
-                    
-                    {/* Text */}
-                    <h2>Expense Tracker</h2>
-                    <p>Please login to your account</p>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    // const [error, setError] = useState('');
 
+    // Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Perform login logic here
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
+    return (
+        <Container
+            fluid 
+            className="
+                bg-dark
+                text-white
+                min-vh-100
+                px-4"
+        >
+            <Row className="
+                d-flex
+                justify-content-center
+                align-items-center"
+            >
+                <Col className="
+                    min-vh-100
+                    d-flex
+                    flex-column
+                    text-center
+                    justify-content-evenly
+                    align-items-strech"
+                    style={{ maxWidth: '600px' }}
+                >
+                    {/* Logo */}
+                    <Container>
+                        <img
+                            src="devmtn.png"
+                            alt="Logo"
+                            className="mt-4"
+                            style={{ width: '100px' }}
+                        />
+                        <h2 >Expense Tracker</h2>
+                        <p className='mt-4'>Please login to your account</p>
+                    </Container>
                     {/* Login Form */}
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group 
+                            className="mb-4"
+                            controlId="formBasicEmail"
+                        >
                             <InputGroup>
                                 <InputGroup.Text>
                                     <FontAwesomeIcon icon={faUser} />
                                 </InputGroup.Text>
-                                <Form.Control type="email" placeholder="Username" />
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    required
+                                />
                             </InputGroup>
                         </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Group 
+                            className="mb-4"
+                            controlId="formBasicPassword"
+                        >
                             <InputGroup>
                                 <InputGroup.Text>
                                     <FontAwesomeIcon icon={faLock} />
                                 </InputGroup.Text>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
                             </InputGroup>
                         </Form.Group>
-
-                        <Button variant="primary" type="submit">
+                        <Button 
+                            variant="primary"
+                            type="submit"
+                            size='lg'
+                        >
                             Login
                         </Button>
                     </Form>
-                    {/* Remember Me and Forgot Password */}
-                    <div className="mt-3 d-flex justify-content-between align-items-center">
-                        <Form.Check 
-                            type="checkbox" 
-                            label="Remember me" 
-                            // className="d-flex"
-                            style={{ textAlign: 'left', gap: '0.5rem' }} 
-                        />
-                        <a href="#">Forgot password?</a>
-                    </div>
                     {/* Sign Up Link */}
-                    <p className="mt-3">Don't have an account? </p>
-                    <Button variant='outline-primary' type='submit'>
-                        Sign Up
-                    </Button>
-
+                    <Container
+                        fluid
+                        className='
+                            d-flex
+                            flex-column
+                            justify-content-between
+                            align-items-center
+                            p-0
+                            mb-5'
+                    >
+                        {/* Remember Me and Forgot Password */}
+                        <Container 
+                            className='
+                                d-flex
+                                flex-row
+                                flex-wrap
+                                justify-content-between
+                                align-items-stretch
+                                p-0
+                                mb-4'
+                        >
+                            <Form.Check
+                                type="checkbox"
+                                label="Remember me"
+                            />
+                            <a href='#'>Forgot password?</a>
+                        </Container>
+                        <p>Don't have an account? </p>
+                        <Button 
+                            variant='outline-primary'
+                            type='submit'
+                            size='lg'
+                        >
+                            Sign Up
+                        </Button>
+                    </Container>
                 </Col>
             </Row>
         </Container>
